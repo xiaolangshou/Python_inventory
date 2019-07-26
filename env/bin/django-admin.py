128c5a6e7aa50a28c5ef3e93da0bd1eac6f47896
xiaolangshou <xiaolangshou@foxmail.com>
2019-07-26 20:06:03 +0800

    add new demo

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/liutao/Python_inventory/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ env/lib/python3.7/site-packages/filer/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .clipboardmodels import *  # noqa
+from .filemodels import *  # noqa
+from .foldermodels import *  # noqa
+from .imagemodels import *  # noqa
+from .thumbnailoptionmodels import *  # noqa
+from .virtualitems import *  # noqa
