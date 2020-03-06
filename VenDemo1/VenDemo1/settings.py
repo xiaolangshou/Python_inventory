6dd6669aba68ef153fac657525de10bb15d3457d
xiaolangshou <xiaolangshou@foxmail.com>
2020-03-06 15:38:14 +0800

    add new code for vendemo1

--- VenDemo1/VenDemo1/settings.py
+++ VenDemo1/VenDemo1/settings.py
@@ -126,5 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') #静态文件本地存放的位置
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static') #静态文件本地存放的位置
